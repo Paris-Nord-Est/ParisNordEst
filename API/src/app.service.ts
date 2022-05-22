88fdec25f8aee799badd68544c88936ae2740ece
Maël <mael.mayon@launchmetrics.com>
2022-05-22 16:07:58 +0200

    Test add image from repo

--- API/src/app.service.ts
+++ API/src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getProductsConf(): { [key: string]: string } {
+    return {
+      laBeige:
+        'https://www.carnets-de-traverse.com/blog/wp-content/uploads/2014/10/Photo24_22A.jpg',
+    };
   }
 }
--- cdn/test.js
+++ cdn/test.js
@@ -1,5 +1,9 @@
 (async () => {
-    console.log("Coucou toi");
-    const data = await fetch("https://smooth-dancers-wash-90-43-241-177.loca.lt");
-    console.log(data);
+    try {
+        const res = await fetch("https://shiny-toys-pull-90-43-241-177.loca.lt/", { headers: { "Bypass-Tunnel-Reminder": "true", Accept: "application/json" } });
+        const data = await res.json();
+        console.log(data);
+    } catch (e) {
+        console.log(e);
+    }
 })();
